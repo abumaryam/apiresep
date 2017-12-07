a7ba2aefa2dab17f64e669e799c27cf78400c5a6
abumaryam <wiradianxiv@mti.gadjahmada.edu>
2017-12-07 22:52:09 +0700

    tambahan comment

--- src/routes/kategori.php
+++ src/routes/kategori.php
@@ -2,7 +2,7 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
-
+//semua kategori
 $app->get('/api/kategori',function(Request $request, Response $response){
 
 	$sql = "SELECT * FROM kategori";
